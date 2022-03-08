25dca37a74db7e959c2dd05ba072c380371611f2
Lake <lake@inkthought.codes>
2022-03-08 09:42:36 +0800

    category

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { readdirSync } = require('fs');
+const { readdirSync, statSync, readdir } = require('fs');
 
 /*
   This is an example for an event.
@@ -12,14 +12,29 @@ module.exports = {
     // This will consider the bot process as 'online' (only if running with pm2)
     if (typeof process.send === 'function') process.send('ready');
 
-    const { interactions, application } = client;
-
-    const interactionFiles = readdirSync('./src/interactions').filter(file => file.endsWith('.js'));
-    for (const file of interactionFiles) {
-      const interaction = require(`../interactions/${file}`);
-      interactions.set(interaction.data.name, interaction);
+      const { interactions, application } = client;
+      const interactionFiles = readdirSync('./src/interactions', {withFileTypes: true}) // .filter(file => file.endsWith('.js'));
+      for (const file of interactionFiles) {
+        try {
+          console.log(`Loading folder ${file.name}...`.bold.cyan);
+          let dir = readdirSync(`./src/interactions/${file.name}`, {withFileTypes: true})
+          for (const file2 of dir) {
+            if (file2.name.endsWith('.js')) {
+              try { 
+                const interaction = require(`../../src/interactions/${file.name}/${file2.name}`);
+                interactions.set(interaction.data.name, interaction);
+                console.log(`Loaded interaction ${interaction.data.name}`.bold.green);
+              } catch (e) {
+                console.log(`Failed to load interaction ${file.name}/${file2.name}`.bold.white);
+                console.log(e)
+            }
+          }
+        }
+      } catch (e) {
+        console.log(`Failed to load interaction ${file}`.bold.red);
+        console.log(e)
+      }
     }
-
     return application.commands?.set([...interactions.map(i => i.data)]);
-  },
+  }
 };
